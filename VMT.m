3767157290eee74e960ee62b768c85189de0bf88
Frank L. Engel <fengel@usgs.gov>
2015-12-10 11:34:18 -0600

    Updated HTML documentation. Ready to merge and retest.

--- VMT.m
+++ VMT.m
@@ -108,7 +108,7 @@ load_prefs(handles.figure1)
 % Initialize the GUI parameters:
 % ------------------------------
 guiparams = createGUIparams;
-guiparams.vmt_version = {'v4.08'; 'r20150924'};
+guiparams.vmt_version = {'v4.08'; 'r20151210'};
 
 % Draw the VMT Background
 % -----------------
