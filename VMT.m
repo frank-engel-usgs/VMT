e1fb0bd28c9f3ede92a37f017402cafa11602d26
Frank L. Engel <fengel@usgs.gov>
2017-01-31 08:44:55 -0600

    For v4.08 r20160131 which fixes issue 349

--- VMT.m
+++ VMT.m
@@ -110,7 +110,7 @@ load_prefs(handles.figure1)
 % Initialize the GUI parameters:
 % ------------------------------
 guiparams = createGUIparams;
-guiparams.vmt_version = {'v4.08'; 'r20161011'};
+guiparams.vmt_version = {'v4.08'; 'r20170131'};
 
 % Draw the VMT Background
 % -----------------
